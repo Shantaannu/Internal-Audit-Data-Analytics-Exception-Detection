71b0de3bbb9905a36432723c44864b495ff70b06
Shantaannu <Shantanududhankar670@gmail.com>
2026-09-06 20:19:19 +0100

    Created  self Approval sql

--- sql/02_self_approval.sql
+++ sql/02_self_approval.sql
@@ -1,4 +1,12 @@
--- Identify transactions where the initiator also approved the transaction
-SELECT *
+SELECT
+    Transaction_ID,
+    Transaction_Date,
+    Account_ID,
+    Employee_ID,
+    Approver_ID,
+    Amount_EUR,
+    Transaction_Type,
+    Branch
 FROM banking_transactions
-WHERE Employee_ID = REPLACE(Approver_ID, 'APR', 'EMP');
+WHERE Employee_ID = REPLACE(Approver_ID, 'APR', 'EMP')
+ORDER BY Amount_EUR DESC;
