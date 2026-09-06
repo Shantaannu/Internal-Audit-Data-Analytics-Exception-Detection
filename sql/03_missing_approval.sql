1c87804bd112564ff1b87324cf2bb28433fb70bd
Shantaannu <Shantanududhankar670@gmail.com>
2026-09-06 20:20:01 +0100

    Created  Missing Approval

--- sql/03_missing_approval.sql
+++ sql/03_missing_approval.sql
@@ -1,5 +1,17 @@
--- Identify transactions requiring review because approval is pending
-SELECT *
-FROM banking_transactions
-WHERE Approval_Status = 'Pending'
-  AND Amount_EUR > 1000;
+SELECT
+    t.Transaction_ID,
+    t.Transaction_Date,
+    t.Transaction_Type,
+    t.Amount_EUR,
+    t.Approval_Status,
+    t.Employee_ID,
+    t.Approver_ID,
+    t.Branch,
+    a.Approval_Required_Above_EUR,
+    a.Required_Approvals
+FROM banking_transactions t
+JOIN approval_thresholds a
+    ON t.Transaction_Type = a.Transaction_Type
+WHERE t.Amount_EUR > a.Approval_Required_Above_EUR
+  AND t.Approval_Status = 'Pending'
+ORDER BY t.Amount_EUR DESC;
